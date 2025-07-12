1f34b7bcfe5fcea3f46cb9fb7c70b3831a9d4277
prince2004patel <princepatel30324@gmail.com>
2025-07-12 15:12:19 +0530

    add admin side

--- admin/src/App.jsx
+++ admin/src/App.jsx
@@ -1,16 +1,8 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+// src/App.jsx
+import AdminDashboard from './components/AdminDashboard'
 
 function App() {
-  const [count, setCount] = useState(0)
-
-  return (
-   <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
-  )
+  return <AdminDashboard />
 }
 
 export default App
